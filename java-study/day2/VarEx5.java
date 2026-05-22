55ba40d3d0eea9b97ac9ffc0a3843963f3559eb1
FAST <FAST@DESKTOP-D7KQLFO>
2026-05-22 09:57:55 +0900

    VarEx5 Commit

---
+++ java-study/day2/VarEx5.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class VarEx5 {
+	public static void main(String[] args) {
+		// 자료형 변수명 = 값;
+		
+		char c1 = 'a'; // 문자 1개만
+		char c2 = 97; // ASCII 코드
+		char c3 = '\u0061'; // UNICODE
+		
+		System.out.println("c1 : " + c1);
+		System.out.println("c2 : " + c2);
+		System.out.println("c3 : " + c3);
+		// ctrl + f11
+		
+		System.out.println("하하하 오늘은 금요일 \n");
+		
+		
+		
+	}
+}
