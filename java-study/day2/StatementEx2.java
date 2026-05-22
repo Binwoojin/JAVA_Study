a9ac9c3d968f905ce5d3cfac6bb7c5b92cbaebfc
WOOJIN <97070372+Binwoojin@users.noreply.github.com>
2026-05-22 06:04:18 +0000

    error checked

---
+++ java-study/day2/StatementEx2.java
@@ -0,0 +1,8 @@
+public class StatementEx2 {
+    public static void main(String[] args) {
+        // 화면에 1부터 100까지 값을 출력
+        for (int i = 1; i<=100; i++) {
+            System.out.println(i);
+        }
+    }
+}
--- java-study/day2/quiz1.java
+++ java-study/day2/quiz1.java
@@ -1,6 +1,6 @@
 public class quiz1 {
     public static void main(String[] args) {
-        for (int i = 0; i<5; i++) {
+        for (int i = 1; i<=5; i++) {
             System.out.println("Hello Java World");
         }        
     }
