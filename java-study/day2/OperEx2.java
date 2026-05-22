3525ca775ce91975f75e5da40af662783f2b8475
WOOJIN <97070372+Binwoojin@users.noreply.github.com>
2026-05-22 03:19:02 +0000

    before half time saved

---
+++ java-study/day2/OperEx2.java
@@ -0,0 +1,19 @@
+public class OperEx2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a + b = "+ (a+b));
+        System.out.println("a - b = "+ (a-b));
+        System.out.println("a * b = "+ (a*b));
+        System.out.println("a / b = "+ (a/b));
+        System.out.println("a % b = "+ (a%b));
+
+
+        byte m1 = 100;
+        short m2 = 15;
+
+        System.out.println("m1 / m2 = " + (m1/m2));
+    }
+    
+}
