a004fd7c195f2e009a7e167939c1f3c684247404
WOOJIN <97070372+Binwoojin@users.noreply.github.com>
2026-05-22 05:21:34 +0000

    4th class example

---
+++ java-study/day2/OperEx8.java
@@ -0,0 +1,9 @@
+public class OperEx8 {
+    public static void main(String[] args) {
+        int a = 10;
+        a++;
+        a += 2; // (a = a+2)
+
+        System.out.println("a = " +a);
+    }
+}
