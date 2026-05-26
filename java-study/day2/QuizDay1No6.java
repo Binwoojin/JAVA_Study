3f9f9809961f5959c85bfef7081dae4c3472a61d
WOOJIN <97070372+Binwoojin@users.noreply.github.com>
2026-05-26 01:02:53 +0000

    rewrited QuizDay1No6

--- java-study/day2/QuizDay1No6.java
+++ java-study/day2/QuizDay1No6.java
@@ -8,12 +8,12 @@ public class QuizDay1No6 {
         int num; // 입력한 숫자를 저장할 수 있는 변수 선언
 
         do {
-            System.out.println("숫자를 입력하세요 : "); // 사용자에게 입력 메시지 출력
+            System.out.print("숫자를 입력하세요 : "); // 사용자에게 입력 메시지 출력 (print() 메서드를 써야함)
             num = sc.nextInt(); // 사용자로부터 숫자 입력 받기
 
             if (num != 5) { // 입력한 숫자가 5가 아닐 경우
                 System.out.println("틀렸습니다."); // "틀렸습니다" 라는 메세지를 출력
-            }
+            }햣
         } while (num != 5); // num 값이 5가 아닐 동안 계속 반복
 
         System.out.println("정답입니다!"); // 반복문 종료 후 정답 메시지 출력
@@ -25,5 +25,5 @@ public class QuizDay1No6 {
 
     // 1. throws IOException의 경우 예외처리 관련으로 에러가 발생할 수 있으니 자바에게 미리 알려주는 것
     // 2. Scanner는 입력 받기 즉, 사용자 입력을 받는 도구로 사용하는 것 키보드, 숫자, 문자열 입력을 받는 도구로 사용
-    
+
 }
